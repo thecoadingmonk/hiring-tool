fc6fad7ab36da5d36405b2ffdf0ac9d89a786a5b
The monk <thecoadingmonk@gmail.com>
2023-03-27 20:05:43 +0000

    refactor: removed wrapper in RadioButton

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -12,30 +12,25 @@ const RadioButton: FC<RadioButtonProps> = ({
   onChange = () => null,
 }: RadioButtonProps) => {
   return (
-    <>
-      <div className="flex items-center">
-        <input
-          type="radio"
-          id={id}
-          name={name}
-          value={value}
-          className="appearance-none inline-block w-5 h-5 p-0.5 bg-clip-content border-2 border-gray-10 rounded-full checked:bg-primary-900"
-          checked={checked}
-          ref={ref}
-          onBlur={onBlur}
-          onChange={onChange}
-        />
-        <label
-          htmlFor={id}
-          className="ml-1 text-font-placeholder font-normal text-sm  
-         
-         
-        "
-        >
-          {label}
-        </label>
-      </div>
-    </>
+    <div className="flex items-center">
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        className="appearance-none inline-block w-5 h-5 p-0.5 bg-clip-content border-2 border-gray-10 rounded-full checked:bg-primary-900"
+        checked={checked}
+        ref={ref}
+        onBlur={onBlur}
+        onChange={onChange}
+      />
+      <label
+        htmlFor={id}
+        className="ml-1 text-font-placeholder font-normal text-sm"
+      >
+        {label}
+      </label>
+    </div>
   );
 };
 
